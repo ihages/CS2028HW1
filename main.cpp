97c5fb06cd69a236d07a8308eadd3f6b29e360c1
EvanOleary26 <43943154+EvanOleary26@users.noreply.github.com>
2025-01-27 15:39:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,45 @@ int main() {
 	std::cout << "What is the file you wish to open?" << std::endl;
 	std::cin >> fileName;
 	std::ifstream textFile(fileName);
+	std::ofstream outputFile("CardCatalog.txt");
+    
+    if (textFile.fail()){
+        std::cout << "Failed to open file" << std::endl;
+        exit(0);
+    } else {
+        std::cout << "Worked!" << std::endl;
+    }
 
+    struct BookInfo{
+        public:
+            std::string title;
+            std::string firstName;
+            std::string lastName;
+            int wordCount;
+            int letterFrequency;
+            int lineCount;
+     };
+
+    BookInfo bookInfo;
+    
+    getline(textFile, bookInfo.title);
+    textFile >> bookInfo.firstName;
+    textFile >> bookInfo.lastName;
+
+    outputFile << "Book Title: " << bookInfo.title << std::endl;
+    outputFile << "Author Name: " << bookInfo.firstName << " " << bookInfo.lastName << std::endl;
+    outputFile << "First Name: " << bookInfo.firstName << std::endl;
+    outputFile << "Last Name: " << bookInfo.lastName << std::endl;
+
+
+     
+
+
+    
+
+    textFile.close();
+    outputFile.close();
+/*
 	if (textFile) {
 		std::cout << "Opening file..." << std::endl;
 
@@ -21,7 +59,7 @@ int main() {
 
 		//editing file
 		
-
+*/
 
 		int letterArray[26] = { 0,0,0,0,0,0,0,0,
 								0,0,0,0,0,0,0,0,
